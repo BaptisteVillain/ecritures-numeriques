c97da962daac6192c92e505511d6c4261da59fb3
bvillain92@gmail.com <BVillain92@gmail.com>
2018-03-26 12:12:57 +0200

    :rocket: highlight event & date time picker

--- functions.php
+++ functions.php
@@ -146,3 +146,68 @@ function custom_menu_order() {
 
 add_filter( 'custom_menu_order', '__return_true' );
 add_filter( 'menu_order', 'custom_menu_order' );
+
+
+/**
+ *
+ * Remove default taxonomies
+ *
+ * @link http://w4dev.com/wp/remove-taxonomy/
+ *
+ */
+ 
+function remove_taxonomy(){
+	global $wp_taxonomies;
+
+	$taxonomies = array( 'category', 'post_tag' );
+	foreach( $taxonomies as $taxonomy ) {
+		if ( taxonomy_exists( $taxonomy) ){
+			unset( $wp_taxonomies[$taxonomy]);
+		}
+	}
+}
+add_action( 'init', 'remove_taxonomy');
+
+
+/**
+ * ACF SELECTED EVENT
+ */
+
+function check_save_event($post_id){
+	
+	$fields = false;
+
+	if(get_post_type($post_id) === 'event'){
+		if(isset($_POST['acf'])){
+		
+		$fields = $_POST['acf'];
+		$highlighted = $fields[field_5ab8ba4dd78d9];
+
+
+		if($highlighted){
+
+			$args = array(
+				'post_type' => 'event',
+				'posts_per_page' => -1,
+				'meta_query' => array(
+					array(
+						'key' => 'highlight_event',
+						'value' => '1',
+						'compare' => '=='
+					)
+				),
+			);
+
+			$query = new WP_Query($args);
+			
+			if($query->have_posts()){
+				while ($query->have_posts()) {
+					$query->the_post();
+					update_field('field_5ab8ba4dd78d9', false, get_the_ID());
+				}
+			}
+		}
+		}
+	}
+}
+add_action('acf/save_post', 'check_save_event', 1);
