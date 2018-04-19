b5eb4b6beb8e3a1c8d07107698b4b5dadd14d3e4
bvillain92@gmail.com <BVillain92@gmail.com>
2018-04-19 15:38:13 +0200

    :rocket: single-publication dynamic content

--- single.php
+++ single.php
@@ -14,15 +14,55 @@ $post = Timber::query_post();
 $context['post'] = $post;
 $context['comment_form'] = TimberHelper::get_comment_form();
 
-$authors = [];
 
-foreach ($post->custom['authors'] as $key => $author) {
-	# code...
+$args = array(
+	'post_type' => array( 'members' ),
+	'orderby' => 'ASC',
+	'post_in' => $post->custom['authors']
+);
+
+
+$parse = parse_url($post->url);
+$context['domain'] = $parse['host'];
+
+$context['authors'] = get_posts(array(
+	'post_type' => 'member',
+	'post__in' => $post->custom['authors']
+));
+
+$research_field = wp_get_post_terms($post->id, 'research_field');
+$context['research_field'] = $research_field[0];
+$context['tags'] = wp_get_post_terms($post->id, array('research_topic', 'axis', 'key_concept'));
+
+
+
+$discovers =  get_posts(array(
+	'post_type' => 'publication',
+	'post__in' => $post->custom['relation_publication_publication']
+));
+
+foreach ($discovers as $key => $publication) {
+	$list_authors = get_post_meta($publication->ID, 'publication_authors', false);
+	$authors = get_posts(array(
+		'post_type' => 'member',
+		'post__in' => $list_authors[0]
+	));
+	$discovers[$key]->authors = $authors;
+
+	$research_field = wp_get_post_terms($publication->ID, 'research_field');
+	$discovers[$key]->research_field = $research_field[0];
+
+	$parse = parse_url($publication->url);
+	$discovers[$key]->domain = $parse['host'];
 }
 
-echo '<pre>';
-print_r($post->custom['authors']);
-echo '</pre>';
+$context['discovers'] = $discovers;
+
+// echo '<pre>';
+// print_r($discovers);
+// echo '</pre>';
+
+
 
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
