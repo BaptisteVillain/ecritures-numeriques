8cda7a158ce93837f0cc0cfa0adbfb99d7679786
bvillain92@gmail.com <BVillain92@gmail.com>
2018-04-29 23:44:12 +0200

    :construction::rocket: single-taxonomy dynamic content

--- taxonomy.php
+++ taxonomy.php
@@ -2,16 +2,44 @@
 /**
  * The Template for displaying all single taxonomies
  *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
  * @package  WordPress
  * @subpackage  Timber
  * @since    Timber 0.1
  */
 
 $context = Timber::get_context();
-$post = Timber::query_post();
-$context['post'] = $post;
+$context['term'] = new TimberTerm();
 
+$context['term']->description = term_description($context['term']->term_id);
+
+$context['term']->related = []; 
+
+$types = array('publication', 'projet', 'event');
+foreach ($types as $key => $type) {
+  $result = get_posts(
+    array(
+      'posts_per_page' => -1,
+      'post_type' => $type,
+      'tax_query' => array(
+        array(
+          'taxonomy' => $context['term']->taxonomy,
+          'field' => 'term_id',
+          'terms' => $context['term']->id
+        )
+      )
+    )
+  );
+  
+  foreach ($result as $key => $item) {
+    $result[$key]->path = get_permalink($item->ID);
+  }
+  if(!empty($result)){
+    $context['term']->related[] = $result;
+  }
+}
+
+// echo '<pre>';
+// print_r($context['term']->related);
+// echo '</pre>';
 
 Timber::render( array( 'taxonomy.twig' ), $context );
