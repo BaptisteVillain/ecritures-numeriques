c9be98ec724429af343912cbfc76f9842a1fd807
bvillain92@gmail.com <BVillain92@gmail.com>
2018-06-03 13:21:28 +0200

    :art: Term controller

--- taxonomy.php
+++ taxonomy.php
@@ -9,10 +9,14 @@
 
 global $params;
 
+$term = new Term($params['term']);
 
-$term = new TimberTerm($params['term']);
+$taxonomies = get_taxonomies(array(
+  'public' => true,
+  '_builtin' => false
+));
 
-if(!$params['term'] || empty($term->name))
+if(!$params['term'] || empty($term->name) || !in_array($term->taxonomy, $taxonomies))
 {
   Timber::render( '404.twig', $context );
   exit;
@@ -20,86 +24,5 @@ if(!$params['term'] || empty($term->name))
 
 $context = Timber::get_context();
 $context['term'] = $term;
-
-
-$context['term']->description = term_description($context['term']->term_id);
-$context['term']->related = [];
-
-/**
- * Get Related Publications, projects and events
- */
-$types = array('publication', 'project', 'event');
-foreach ($types as $key => $type) {
-
-  $args = array(
-  'posts_per_page' => -1,
-  'post_type' => $type,
-  'tax_query' => array(
-    array(
-      'taxonomy' => $context['term']->taxonomy,
-      'field' => 'term_id',
-      'terms' => $context['term']->id
-    )
-  ));
-
-  if($type === 'publication'){
-    $args['meta_query'] = array(
-      array(
-        'key' => 'private',
-        'compare' => '!=',
-        'value' => '1'
-      )
-    );
-  }
-
-  $result = Timber::get_posts($args);
-  
-  foreach ($result as $key => $item) {
-    $result[$key]->path = get_permalink($item->ID);
-  }
-
-  if(!empty($result)){
-    $context['term']->related[] = $result;
-  }
-}
-
-/**
- * Get previous and next taxonomies
- */
-
-$taxonomies = array('research_field',  'axis', 'research_topic', 'key_concept');
-$terms = array();
-foreach ($taxonomies as $key => $taxonomy) {
-  $term = get_terms(array(
-    "taxonomy" => $taxonomy,
-    "hide_empty" => false
-  ));
-
-  $terms = array_merge($terms, $term);
-}
-
-$index = 0;
-foreach ($terms as $key => $term) {
-  if($term->slug == $context['term']->slug){
-    $index = $key;
-  }
-}
-
-if($index > 0){
-  $context['previous'] = $terms[$index - 1];
-}
-else {
-  $context['previous'] = $terms[count($terms) - 1];  
-}
-
-if ($index < count($terms) - 1) {
-  $context['next'] = $terms[$index + 1];
-}
-else{
-  $context['next'] = $terms[0];
-}
-
-$context['next']->path = term_link($context['next']);
-$context['previous']->path = term_link($context['previous']);
 
 Timber::render( array( 'single-rubric.twig' ), $context );
