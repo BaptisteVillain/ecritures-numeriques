9d12ae418ea9df1c5e7f18b2277f6ee18fb709e9
bvillain92@gmail.com <BVillain92@gmail.com>
2018-06-11 00:04:56 +0200

    :rocket: use acf wysiwyg

--- inc/remove-filters.php
+++ inc/remove-filters.php
@@ -1,5 +1,25 @@
 <?php
 
 remove_filter('the_content', 'wpautop');
-remove_filter( 'the_excerpt', 'wpautop');
+remove_filter('the_excerpt', 'wpautop');
 remove_filter('term_description','wpautop');
+
+function acf_replace_p($content) {
+  $content = preg_replace("/<p[^>]*?>/", "", $content);
+  $content = str_replace("</p>", "<br />", $content);
+
+  return $content;
+}
+
+function acf_wrapp_ytb($content) {
+  $content = str_replace("<iframe", '<div class="content__youtube"><iframe', $content);
+  $content = str_replace("</iframe>", '</iframe></div>', $content);
+
+  return $content;
+}
+
+function acf_wysiwyg(){
+  add_filter('acf_the_content', 'acf_wrapp_ytb');
+  add_filter('acf_the_content', 'acf_replace_p');  
+}
+add_action('acf/init', 'acf_wysiwyg', 15);
