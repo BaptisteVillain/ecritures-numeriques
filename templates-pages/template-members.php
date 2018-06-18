0adde64d15a9760552b152a0c2ced75501fa1819
bvillain92@gmail.com <BVillain92@gmail.com>
2018-06-18 04:20:41 +0200

    members dynamic content

--- templates-pages/template-members.php
+++ templates-pages/template-members.php
@@ -6,5 +6,25 @@
 
 $context = Timber::get_context();
 
+$context['categories'] = array();
+
+$categories = Timber::get_terms(array(
+  'taxonomy' => 'post',
+  'parent' => 0
+));
+
+foreach ($categories as $category) {
+  $context['categories'][] = new Timber\PostQuery(array(
+    'post_type' => 'member',
+    'tax_query' => array(
+      array(
+        'taxonomy' => 'post',
+        'field' => 'slug',
+        'terms' => $category->slug
+      )
+    )
+  ));
+}
+
 Timber::render( 'members.twig', $context );
  
