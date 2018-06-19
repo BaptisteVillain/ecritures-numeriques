9b09c1ba5d6230c8bbf3a61bcf5f129b991c9b99
thaelene <helene.ta@hetic.net>
2018-06-19 23:06:01 +0200

    Style correction projects, single project, single rubrics

--- templates-pages/template-projects.php
+++ templates-pages/template-projects.php
@@ -11,7 +11,7 @@ $args = array(
 'post_type' => 'project',
 // Get all posts
 'posts_per_page' => -1,
-// Order by post date
+// Order by index post
 'orderby' => array('id_number'), 
 'order' => 'ASC'
 );
@@ -22,7 +22,13 @@ foreach ($context['projects'] as $key => $project) {
   $context['projects'][$key]->cover = get_field('cover_image', $project->ID);
   $context['projects'][$key]->description = get_field('content', $project->ID);
   $context['projects'][$key]->index = get_field('id_number', $project->ID);
+  $context['projects'][$key]->link = get_permalink($project->ID);
 }
+
+// echo '<pre>';
+// print_r($context['projects']);
+// echo '</pre>';
+// exit;
 
 Timber::render( 'projects.twig', $context );
  
