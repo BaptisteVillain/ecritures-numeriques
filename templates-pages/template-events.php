dd79a31a3a3982b7df7a4103d175838c2551058f
thaelene <helene.ta@hetic.net>
2018-06-19 02:15:10 +0200

    Events partials, missing style

--- templates-pages/template-events.php
+++ templates-pages/template-events.php
@@ -13,13 +13,46 @@ $args = array(
 'posts_per_page' => -1,
 // Order by post date
 'orderby' => array(
+    'date_end' => 'DESC'
+),
+'meta_query' => array(
+        array(
+            'key' => 'date_end',
+            'value' => date('Y-m-d'),
+            'compare' => '>=',
+            'type' => 'DATE'
+        )
+    ),
+);
+
+$args_past = array(
+// Get post type project
+'post_type' => 'event',
+// Get all posts
+'posts_per_page' => -1,
+// Order by post date
+'orderby' => array(
     'date' => 'DESC'
-));
+),
+'meta_query' => array(
+        array(
+            'key' => 'date_end',
+            'value' => date('Y-m-d'),
+            'compare' => '<=',
+            'type' => 'DATE'
+        )
+    ),
+);
 
 $context['events'] = Timber::get_posts( $args );
+$context['events_past'] = Timber::get_posts( $args_past );
 
 foreach ($context['events'] as $key => $event) {
   $context['events'][$key]->cover = get_field('cover_image', $event->ID);
+}
+
+foreach ($context['events_past'] as $key => $event) {
+  $context['events_past'][$key]->cover = get_field('cover_image', $event->ID);
 }
 
 // echo '<pre>';
