a949740842d8b07daab38656b74b3339d0924060
bvillain92@gmail.com <BVillain92@gmail.com>
2018-06-20 15:36:54 +0200

    :bug: projects css

--- routes/routes.php
+++ routes/routes.php
@@ -4,3 +4,8 @@ Routes::map('rubriques/:term', function($params){
   
   Routes::load('taxonomy.php', $params, null, 200);
 });
+
+Routes::map('publications/:term', function($params){
+  
+  Routes::load('./templates-pages/template-publications.php', $params, null, 200);
+});
--- templates-pages/template-publications.php
+++ templates-pages/template-publications.php
@@ -3,8 +3,62 @@
  * Template Name: Publications Page
  * Description: All Publications
  */
+global $params;
+global $post;
 
 $context = Timber::get_context();
+$context['page'] = new Timber\Post(7475);
 
-Timber::render( 'front-page.twig', $context );
- 
+$context['selected'] = array();
+if(!empty($context['page']->selected_research_field)){
+  $context['selected'] = array_merge($context['selected'], $context['page']->selected_research_field);
+}
+if(!empty($context['page']->selected_research_topic)){
+  $context['selected'] = array_merge($context['selected'], $context['page']->selected_research_topic);
+}
+if(!empty($context['page']->selected_key_concept)){
+  $context['selected'] = array_merge($context['selected'], $context['page']->selected_key_concept);
+}
+if(!empty($context['page']->selected_axes)){
+  $context['selected'] = array_merge($context['selected'], $context['page']->selected_axes);
+}
+
+
+foreach ($context['selected'] as $key => $term) {
+  $context['selected'][$key] = new Timber\Term($term);
+}
+
+function get_selected($slug, $array){
+  foreach ($array as $key => $term) {
+    if($term->slug === $slug){
+      return $term;
+    }
+  }
+
+  return false;
+}
+
+$context['term'] = get_selected($params['term'], $context['selected']);
+
+if(!$context['term']){
+  $args = array(
+    'post_type' => 'publication',
+    'posts_per_page' => -1
+  );
+} else {
+  $args = array(
+    'post_type' => 'publication',
+    'posts_per_page' => -1,
+    'tax_query' => array(
+      array(
+        'taxonomy' => $context['term']->taxonomy,
+        'field' =>  'slug',
+        'terms' =>  $context['term']->slug
+      )
+    )
+  );
+}
+
+$context['posts'] = new Timber\PostQuery($args);
+
+Timber::render( 'publications.twig', $context );
